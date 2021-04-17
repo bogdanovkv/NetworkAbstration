db1d9bc8036f0ba3362c2904e7992372bd435c41
Konstatin Bogdanov <>
2021-04-17 20:13:46 +0300

    fix error in name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "NetworkAbstration",
+    name: "NetworkAbstraction",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "NetworkAbstration",
-            targets: ["NetworkAbstration"]),
+            name: "NetworkAbstraction",
+            targets: ["NetworkAbstraction"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "NetworkAbstration",
+            name: "NetworkAbstraction",
             dependencies: []),
         .testTarget(
-            name: "NetworkAbstrationTests",
-            dependencies: ["NetworkAbstration"]),
+            name: "NetworkAbstractionTests",
+            dependencies: ["NetworkAbstraction"]),
     ]
 )
---
+++ Tests/NetworkAbstractionTests/NetworkAbstrationTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import NetworkAbstration
+
+final class NetworkAbstrationTests: XCTestCase {
+    func testExample() {
+
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
